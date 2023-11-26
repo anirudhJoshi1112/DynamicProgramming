018a2de26c2486f51745c19ffbfeff9957828239
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-11-26 11:12:21 +0530

    Nth fibonacci number

---
+++ dp3.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n,vector<int> &dp){
+        if(n<=1){
+                return n;
+        }
+        if(dp[n]!=-1){
+                return dp[n];
+        }
+        dp[n]=fib(n-1,dp)+fib(n-2,dp);
+        return dp[n];
+}
+
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+        vector<int> dp(n+1);
+        for(int i=0;i<=n;i++){
+                dp[i]=-1;
+        }
+        cout<<fib(n,dp);
+        return 0;
+}
